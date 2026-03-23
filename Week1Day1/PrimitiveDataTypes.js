a445472f14cc236e46249c00c9d6a667be945a1f
Santhiya98study <sandymaha798@gmail.com>
2026-03-23 07:34:58 +0530

    Add files via upload
    Week1Day1

---
+++ Week1Day1/PrimitiveDataTypes.js
@@ -0,0 +1,23 @@
+//String Datatype
+let username="Santhiya"
+console.log(username);
+console.log(typeof username);
+
+let organization="Testleaf"
+console.log(organization);
+console.log(typeof organization);
+
+//Number 
+let contactnumber=9876543210
+console.log(contactnumber);
+console.log(typeof contactnumber);
+
+//Boolean
+let knowsAutomation=true
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+//Undefined
+let usesPlaywright;
+console.log(usesPlaywright);  //If we didnot give value it is undefined
+console.log(typeof usesPlaywright);
